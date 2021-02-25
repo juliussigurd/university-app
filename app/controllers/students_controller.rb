da8a62c684f0c33099e36564686534c70445840d
julius heilmann <juliussigurd@live.dk>
2021-02-25 17:53:10 +0100

    add create students functionality and started on error handling

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,22 @@ class StudentsController < ApplicationController
     def new
         @student = Student.new
     end
+
+    def create
+        @student = Student.new(student_params)
+        if @student.save
+            # session[:student_id] = @student.id
+            flash[:notice] = "Welcome to Tech university, you have successfully been signed up"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:name, :email)
+    end
     
 end
